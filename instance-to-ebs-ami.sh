71548e1a556f4fcf5914b1f685ef60da6d2d0b39
Enis Afgan <afgane@gmail.com>
2012-01-24 13:13:40 +0100

    Add verbosity to instance-to-ebs-ami.sh

--- instance-to-ebs-ami.sh
+++ instance-to-ebs-ami.sh
@@ -6,16 +6,19 @@ EBS_MOUNT_POINT='/mnt/ebs'
 IMAGE_DEVICE='/dev/sdj' # Temporary vol used to copy the root file system onto during rebundling
 IMAGE_MOUNT_POINT='/mnt/tmp_ebs'
 
+echo "Creating XFS file system on device $IMAGE_DEVICE (temp vol)"
 mkfs.xfs -f $IMAGE_DEVICE
 if [ -d $IMAGE_MOUNT_POINT ]; then
     rm -rf $IMAGE_MOUNT_POINT
 fi
 mkdir -m 000 -p $IMAGE_MOUNT_POINT
 mount -t xfs $IMAGE_DEVICE $IMAGE_MOUNT_POINT
+echo "Mounted device $IMAGE_DEVICE to $IMAGE_MOUNT_POINT"
 
 # Newer Ubuntu images use a 'LABEL=uec-rootfs' for the root file system device 
 # so check if it's present in /etc/fstab and create the future root file system 
 # using that label
+echo "Creating XFS file system on device $EBS_DEVICE (future image vol)"
 if grep -q "uec-rootfs" /etc/fstab; then
     mkfs.xfs -f -L uec-rootfs $EBS_DEVICE
 else 
@@ -23,15 +26,18 @@ else
 fi
 mkdir -m 000 -p $EBS_MOUNT_POINT
 mount -t xfs $EBS_DEVICE $EBS_MOUNT_POINT
+echo "Mounted device $EBS_DEVICE to $EBS_MOUNT_POINT; starting rsync"
 
-#make a local working copy
-rsync --stats -ax --exclude /root/.bash_history --exclude /home/*/.bash_history --exclude /etc/ssh/ssh_host_* --exclude /etc/ssh/moduli --exclude /etc/udev/rules.d/*persistent-net.rules --exclude /var/lib/ec2/* --exclude=/mnt/* --exclude=/proc/* --exclude=/tmp/* --exclude /root/.ssh/* --exclude /home/ubuntu/.ssh/* --exclude /var/lib/rabbitmq/mnesia / $IMAGE_MOUNT_POINT
+# Make a local working copy
+rsync --stats -avx --exclude /root/.bash_history --exclude /home/*/.bash_history --exclude /etc/ssh/ssh_host_* --exclude /etc/ssh/moduli --exclude /etc/udev/rules.d/*persistent-net.rules --exclude /var/lib/ec2/* --exclude=/mnt/* --exclude=/proc/* --exclude=/tmp/* --exclude /root/.ssh/* --exclude /home/ubuntu/.ssh/* --exclude /var/lib/rabbitmq/mnesia / $IMAGE_MOUNT_POINT
 
 # Because we're using xfs as the root file system, edit /etc/fstab on the image to reflect so
+echo "rsych complete; updating $IMAGE_MOUNT_POINT/etc/fstab"
 sed 's/ext3/xfs /' $IMAGE_MOUNT_POINT/etc/fstab > $IMAGE_MOUNT_POINT/etc/fstab2
 mv $IMAGE_MOUNT_POINT/etc/fstab2 $IMAGE_MOUNT_POINT/etc/fstab
 
-#clear out log files
+# Clear out log files
+echo "Clearing out any log files from $IMAGE_MOUNT_POINT/var/log"
 cd $IMAGE_MOUNT_POINT/var/log
 for i in `ls ./**/*`; do
   echo $i && echo -n> $i
@@ -40,9 +46,15 @@ done
 # > $IMAGE_MOUNT_POINT/root/.ssh/authorized_keys
 # > $IMAGE_MOUNT_POINT/home/ubuntu/.ssh/authorized_keys
 
+echo "Doing the tar trickery"
 cd $IMAGE_MOUNT_POINT
 tar -cpSf - -C ./ . | tar xf - -C $EBS_MOUNT_POINT
 #NOTE, You could rsync / directly to EBS_MOUNT_POINT, but this tar trickery saves some space in the snapshot
 
-umount $EBS_MOUNT_POINT
-umount -lf $IMAGE_MOUNT_POINT
+# Flush all pending write ops and unmount
+echo "Unmounting $EBS_MOUNT_POINT (future image vol)"
+sync;sync;sync;sync && umount $EBS_MOUNT_POINT
+echo "Unmounting $IMAGE_MOUNT_POINT (temp vol)"
+sync;sync;sync;sync && umount -lf $IMAGE_MOUNT_POINT
+
+echo "Script $0 done."
