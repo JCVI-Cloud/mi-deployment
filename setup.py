7016a8391197a7b752ae11041aeab4091f06cf1b
John Chilton <jmchilton@gmail.com>
2012-07-16 23:42:58 -0500

    Restructured package creation to handle missing __init__.py files, now files get copied into a new lib directory with correct package structures and then setup.py targets that.

--- setup.py
+++ setup.py
@@ -2,16 +2,44 @@
 """
 """
 from setuptools import setup, find_packages
+from distutils.command.build import build as distutils_build
+from os import utime, makedirs
+from os.path import join, isdir
+from shutil import copytree, copy, rmtree
+
+
+def setup_package_directory(directory):
+    target_dir = join('lib', directory)
+    try:
+        makedirs(target_dir)
+    except:
+        pass
+    init_path = join(target_dir, '__init__.py')
+    with file(init_path, 'a'):
+        utime(init_path, None)
+
+rmtree("lib")
+package_dir=join("lib","mi_deployment")
+setup_package_directory("mi_deployment")
+for file_to_copy in ['data_fabfile.py', 'mi_fabfile.py', 'tools_fabfile.py', 'volume_manipulations_fab.py', 'util', 'tools']:
+    if isdir(file_to_copy):
+        copy_func = copytree
+    else:
+        copy_func = copy
+    copy_func(file_to_copy, join("lib", "mi_deployment", file_to_copy))
+setup_package_directory(join("mi_deployment", "tools"))
+
 
 setup(name="mi-deployment",
       version="0.1",
       description="Set of fabric scripts used to automate the configuring and and deployment of Galaxy.",
       author="Enis Afgan",
-      packages=["mi_deployment", "mi_deployment.util", "mi_deployment.tools"],
+      packages=["mi_deployment", 
+                "mi_deployment.util", 
+                "mi_deployment.tools",
+                "mi_deployment.tools.util"],
       package_dir = {
-        "mi_deployment": ".",
-        "mi_deployment.util": "./util",
-        "mi_deployment.tools": "./tools",
+        "mi_deployment": "lib/mi_deployment",
       },
       url="https://bitbucket.org/afgane/mi-deployment/",
       classifiers=[
